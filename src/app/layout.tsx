6f22c09435016aaaaf31488949180eb12c067ea3
laamphan <41721718+laamphan@users.noreply.github.com>
2024-06-26 21:34:51 +0700

    Add uploadthing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
+import "@uploadthing/react/styles.css";
 import "~/styles/globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { GeistSans } from "geist/font/sans";
+import { NextSSRPlugin } from "node_modules/@uploadthing/react/next-ssr-plugin/index.cjs";
+import { extractRouterConfig } from "uploadthing/server";
 import { TopNav } from "./_components/topnav";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 export const metadata = {
   title: "T3 Gallery",
@@ -18,6 +22,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable} flex-col gap-4`}>
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
+
         <body>
           <TopNav />
           {children}
